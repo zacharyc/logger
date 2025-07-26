276e2a50a25c90b017c24ecf61491ab35c3c8f81
Zachary Cohen <zachary@zacharyc.com>
2025-07-26 12:31:29 -0400

    first commit

--- Logger/LogItem.swift
+++ Logger/LogItem.swift
@@ -9,10 +9,12 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
+final class LogItem {
+    var note: String
     var timestamp: Date
     
-    init(timestamp: Date) {
+    init(timestamp: Date, note: String) {
+        self.note = note
         self.timestamp = timestamp
     }
 }
